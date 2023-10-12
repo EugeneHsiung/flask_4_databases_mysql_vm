b6bc47d140f0160989e332adaa91f0bae2f7949c
EugeneHsiung <Eugene.Hsiung@stonybrook.edu>
2023-10-12 04:16:59 +0000

    flask

---
+++ mysql/heart.sql
@@ -0,0 +1,13 @@
+create database heart; 
+use heart; 
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE
+);
+
+CREATE TABLE medications (
+    medication_id INT PRIMARY KEY AUTO_INCREMENT,
+    medication_name VARCHAR(100) NOT NULL
+);
---
+++ mysql/populate.sql
@@ -0,0 +1,9 @@
+INSERT INTO patients (first_name, last_name, date_of_birth) VALUES 
+('john', 'smith', '1921-10-15'),
+('kait', 'mom', '1911-10-17'),
+('bob', 'hum', '1931-10-20');
+
+INSERT INTO medications (medication_id,medication_name) VALUES
+(1, 'ibuprofen'), 
+(2, 'albuterol'), 
+(3, 'laxis'); 
